0e27c9b35eef3b4537ba74adde5df54cc20a22b6
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-06-13 10:54:04 +0800

    add member to WarningBox struct

--- DetectTarget/WarningBox.hpp
+++ DetectTarget/WarningBox.hpp
@@ -1,15 +1,18 @@
 #pragma once
 #include <cstdint>
+#include <core/core.hpp>
 
 struct WarningBox
 {
-	WarningBox() : blockX(-1), blockY(-1), timeLeft(0)
+	WarningBox() : blockX(-1), blockY(-1), timeLeft(0), targetRect(0, 0, 0, 0)
 	{
 	}
 
 	int blockX;
 	int blockY;
 	uint32_t timeLeft;
+	cv::Rect targetRect;
+
 };
 
 const auto MaxWarningBoxCount = 2;
