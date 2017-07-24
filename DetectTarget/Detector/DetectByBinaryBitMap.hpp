71de7006200c2cbf3ef104b2aa45a3a8370a7624
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-07-24 17:22:08 +0800

    update detectbybinarybitmap

--- DetectTarget/Detector/DetectByBinaryBitMap.hpp
+++ DetectTarget/Detector/DetectByBinaryBitMap.hpp
@@ -4,34 +4,35 @@
 #include "../Models/FourLimits.hpp"
 #include "../Models/FieldType.hpp"
 
+template <typename DataType>
 class DetectByBinaryBitMap
 {
 public:
-
-	static void Detect(cv::Mat curFrame);
+	static void Detect(cv::Mat frame);
 
 private:
-
 	static int GetBitMap(const cv::Mat& binaryFrame, cv::Mat& bitMap);
 };
 
-inline void DetectByBinaryBitMap::Detect(cv::Mat curFrame)
+template <typename DataType>
+void DetectByBinaryBitMap<DataType>::Detect(cv::Mat frame)
 {
 	cv::Mat binaryFrame;
-	curFrame.copyTo(binaryFrame);
-	Util::BinaryMat(binaryFrame);
+	frame.copyTo(binaryFrame);
+	Util<DataType>::BinaryMat(binaryFrame);
 
 	cv::Mat bitMap(cv::Size(binaryFrame.cols, binaryFrame.rows), CV_32SC1, cv::Scalar(-1));
 	auto totalObject = GetBitMap(binaryFrame, bitMap);
 
 	std::vector<FourLimits> allObjects(totalObject);
-	Util::GetRectangleSize(bitMap, allObjects);
+	Util<DataType>::GetRectangleSize(bitMap, allObjects);
 
-	Util::ShowAllObject(curFrame, allObjects);
-	Util::ShowAllCandidateTargets(curFrame, allObjects);
+	Util<DataType>::ShowAllObject(frame, allObjects);
+	Util<DataType>::ShowAllCandidateTargets(frame, allObjects);
 }
 
-inline int DetectByBinaryBitMap::GetBitMap(const cv::Mat& binaryFrame, cv::Mat& bitMap)
+template <typename DataType>
+int DetectByBinaryBitMap<DataType>::GetBitMap(const cv::Mat& binaryFrame, cv::Mat& bitMap)
 {
 	auto currentIndex = 0;
 	for (auto r = 0; r < binaryFrame.rows; ++r)
@@ -43,9 +44,8 @@ inline int DetectByBinaryBitMap::GetBitMap(const cv::Mat& binaryFrame, cv::Mat& 
 			if (bitMap.at<int32_t>(r, c) != -1)
 				continue;
 
-			Util::FindNeighbor(binaryFrame, bitMap, r, c, currentIndex++, FieldType::Eight);
+			Util<DataType>::FindNeighbor(binaryFrame, bitMap, r, c, currentIndex++, FieldType::Eight);
 		}
 	}
 	return currentIndex;
 }
-
