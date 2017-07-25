476d0ef54dcf35a1e4f5e97b3ea077c50c445069
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-07-25 16:33:52 +0800

    add time printer

--- DetectTarget/LogPrinter/LogPrinter.hpp
+++ DetectTarget/LogPrinter/LogPrinter.hpp
@@ -8,33 +8,55 @@ class LogPrinter
 public:
 	static void PrintLogs(std::string text, LogLevel logLevel);
 
+private:
+	static void printCurrentTime();
 };
 
 inline void LogPrinter::PrintLogs(std::string text, LogLevel logLevel)
 {
+	printCurrentTime();
+
 	switch (logLevel)
 	{
 	case LogLevel::Error:
-	{
-		std::cout << "ERROR => ";
-		break;
-	}
+		{
+			std::cout << "ERROR => ";
+			break;
+		}
 	case LogLevel::Info:
-	{
-		std::cout << "INFO => ";
-		break;
-	}
+		{
+			std::cout << "INFO => ";
+			break;
+		}
 	case LogLevel::Waring:
-	{
-		std::cout << "WARNING => ";
-		break;
-	}
+		{
+			std::cout << "WARNING => ";
+			break;
+		}
 	default:
-	{
-		std::cout << "Not Defined => ";
-		break;
-	};
+		{
+			std::cout << "Not Defined => ";
+			break;
+		};
 	}
 
 	std::cout << text << std::endl;
+}
+
+inline void LogPrinter::printCurrentTime()
+{
+	const auto bufferSize = 20;
+
+	auto timer = time(nullptr);
+	auto currentTime = localtime(&timer);
+
+	char timeStr[bufferSize];
+	sprintf_s(timeStr, bufferSize, "%d-%02d-%02d %02d:%02d:%02d",
+	          currentTime->tm_year + 1900,
+	          currentTime->tm_mon + 1,
+	          currentTime->tm_mday,
+	          currentTime->tm_hour,
+	          currentTime->tm_min,
+	          currentTime->tm_sec);
+	std::cout << timeStr << " ";
 }
